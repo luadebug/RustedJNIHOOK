82e68905f0f8869900f4792ea9d26731e95eb19f
さくらみこ <lin@sz.cn.eu.org>
2025-05-17 15:16:40 +0300

    support only jnihook.h bindings

--- build.rs
+++ build.rs
@@ -14,8 +14,7 @@ fn main() {
     println!("cargo:rustc-link-lib=jvm"); // Link against jvm.lib
     println!("cargo:rustc-link-lib=jawt"); // Link against jawt.lib
 
-
-    // Configure and generate bindings.
+        // Configure and generate bindings.
     let bindings = bindgen::Builder::default()
         .clang_arg("-I3rd/build_MT/_deps/openjdk-src/include")
         .clang_arg("-I3rd/build_MT/_deps/openjdk-src/include/win32")
@@ -35,5 +34,27 @@ fn main() {
     let out_path = PathBuf::from(".");
     bindings
         .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    // Configure and generate bindings.
+    let bindings = bindgen::Builder::default()
+        .clang_arg("-I3rd/build_MT/_deps/openjdk-src/include")
+        .clang_arg("-I3rd/build_MT/_deps/openjdk-src/include/win32")
+        // .clang_arg("-std=c++2b")
+        // The input header we would like to generate
+        // bindings for.
+        .header("3rd/include/jnihook.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the generated bindings to an output file.
+    let out_path = PathBuf::from(".");
+    bindings
+        .write_to_file(out_path.join("lite-bindings.rs"))
         .expect("Couldn't write bindings!");
 }
